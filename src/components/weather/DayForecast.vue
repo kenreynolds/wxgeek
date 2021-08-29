7aace442187e6de6cd41cc7142c6719a1814ccc1
Ken Reynolds <kenneth.reynolds@aa.com>
2021-08-29 11:59:17 -0500

    Redesign daily forecast

--- src/components/weather/DayForecast.vue
+++ src/components/weather/DayForecast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="day row">
+  <div class="day row mb-3">
     <div class="date">
       {{ formatDay(day.dt) }}
     </div>
@@ -28,14 +28,14 @@
 </template>
 
 <script>
- import dayjs from 'dayjs';
+  import dayjs from 'dayjs';
 
   export default {
     name: 'DayForecast',
     props: ['day'],
     methods: {
       displayIcon(icon) {
-        return require(`../../assets/icons/white/${icon}.svg`);
+        return require(`../../assets/icons/gray/${icon}.svg`);
       },
       formatDay(date) {
         return dayjs.unix(date).format('ddd');
@@ -50,16 +50,13 @@
 <style lang="scss" scoped>
   .day {
     align-items: center;
-    border-bottom: 2px solid #0277bd;
+    background-color: #B3E5FC;
+    border: 1px solid #E1F5FE;
+    border-radius: 8px;
     display: flex;
+    height: 60px;
     justify-content: space-between;
-    padding: 1.25rem .75rem .5rem;
-
-    > .condition,
-    > .date,
-    > .temperatures {
-      padding-bottom: 0.75rem;
-    }
+    padding-left: 12px;
 
     &:last-child {
       border: 0;
@@ -82,24 +79,29 @@
     }
 
     .date {
+      font-weight: 700;
       text-align: left;
       width: 32px;
     }
 
     .temperatures {
       align-items: center;
+      background-color: white;
+      border-radius: 0 8px 8px 0;
       display: flex;
       font-weight: 700;
-      justify-content: space-between;
-      width: 85px;
+      height: 59px;
+      justify-content: space-around;
+      padding: 0 8px;
+      width: 96px;
 
       > .high {
         margin-right: 12px;
       }
 
       .fas {
-        color: #B3E5FC;
         margin-right: 2px;
+        opacity: 0.75;
       }
     }
   }
--- src/views/DailyForecast.vue
+++ src/views/DailyForecast.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="daily-forecast">
+  <section class="daily-forecast">
     <DayForecast
       v-for="(day, i) in theDailyForecast"
       :day="day"
       :key="i"
     ></DayForecast>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -26,47 +26,11 @@
 
 <style lang="scss" scoped>
   .daily-forecast {
-    background: linear-gradient(#01579B, #0277bd);
-    height: 100vh;
-    margin: 0 auto;
-    min-width: 275px;
-  }
-
-  .page-header {
-    align-items: center;
     color: #757575;
-    display: flex;
     justify-content: space-between;
-    margin: 0 1rem 2.5rem 1rem;
-    padding-top: 1rem;
-
-    > .back-link {
-       align-items: center;
-      background-color: #FAFAFA;
-      border: 0;
-      border-radius: 100%;
-      box-shadow: 0 2px 4px #BDBDBD;
-      color: #757575;
-      display: flex;
-      height: 25px;
-      justify-content: center;
-      margin: 0;
-      padding: 6px;
-      width: 25px;
-
-      &:hover {
-        color: inherit;
-      }
-    }
-
-    > .site-menu {
-      color: #757575;
-      margin-right: -10px;
-      width: 24px;
-    }
-
-    .fa-calendar-alt {
-      margin-right: 8px;
-    }
+    margin: 0 auto;
+    max-width: 325px;
+    min-width: 275px;
+    padding: 0 4px;
   }
 </style>
